1b27fee7391a8311d39eb63d2edcb28ad0f4914f
tunde <icepaigez@gmail.com>
2021-09-02 16:11:15 +0100

    feat:setting up the dao token

---
+++ src/contracts/SNTCHToken.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SNTCHToken is ERC20, Ownable {
+
+} 
