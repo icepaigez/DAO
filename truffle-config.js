6108c866981f8448cfccb97059e7677c7b304c0f
tunde <icepaigez@gmail.com>
2021-09-03 20:14:26 +0100

    feat: added polygon deploy config settings

--- truffle-config.js
+++ truffle-config.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const mnemonic = process.env.MNEMONIC;
 const endpointUrl = `wss://kovan.infura.io/ws/v3/${process.env.WEB3_INFURA_PROJECT_ID}`
 const etherscanToken = process.env.ETHERSCAN_TOKEN;
-
+const maticEndPoint = `https://polygon-mumbai.infura.io/v3/${process.env.WEB3_INFURA_PROJECT_ID}`
 
 module.exports = {
   networks: {
@@ -27,6 +27,20 @@ module.exports = {
       gas: 5000000,
       gasPrice: 25000000000,
       network_id: 42
+    },
+    matic: {
+      provider: () => new HDWalletProvider({
+        mnemonic: {
+          phrase: mnemonic
+        },
+        providerOrUrl: maticEndPoint
+      }),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      gas: 6000000,
+      gasPrice: 10000000000
     }
   },
   contracts_directory: './src/contracts/',
