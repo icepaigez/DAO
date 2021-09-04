9d32e3fd62f8cb3139a5a15c67583628fd939d6c
tunde <icepaigez@gmail.com>
2021-09-04 11:16:12 +0100

    feat: added chainlink price feed to the contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,10 +5,25 @@ const name = 'SNTCH Token'
 const symbol = 'SNTCH'
 const totalSupply = '100000000000000000000000000'  //100m tokens
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network) {
   await deployer.deploy(SNTCHToken, name, symbol, totalSupply);
-  await deployer.deploy(SntchDao);
+
+  let chainlink_aggr;
+
+  if (network === 'matic') {
+    chainlink_aggr = '0x0715A7794a1dc8e42615F059dD6e406A6594651A'
+    await deployer.deploy(SntchDao, chainlink_aggr);
+  } else if (network === 'kovan') {
+    chainlink_aggr = '0x9326BFA02ADD2366b30bacB125260Af641031331'
+    await deployer.deploy(SntchDao, chainlink_aggr);
+  }
   
-  const sntch = await SNTCHToken.deployed()
+  
+  const sntch = await SNTCHToken.deployed();
+  const dao = await SntchDao.deployed();
+  let ethUsdPrice = await dao.getLatestEthPrice();
+  ethUsdPrice = (Number(web3.utils.fromWei(String(ethUsdPrice))) * 10**18) / 10**8
+
+  console.log('the current ETH price in USD is >>', ethUsdPrice)
 
 };
--- src/contracts/SntchDao.sol
+++ src/contracts/SntchDao.sol
@@ -2,17 +2,37 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract SntchDao is Ownable {
+
+	AggregatorV3Interface internal priceFeed;
 	
 	using SafeMath for uint256;
 
 	string public name = 'SNTCH DAO';
-	uint256 public whitelistedNumber = 0;
+	uint256 public tokenPriceInWei = 2500000000000000000; //$2.5 in wei
 
 	mapping (address => bool) whitelist;
     mapping (address => bool) blacklist;
 
     event Whitelisted(address addr, bool status);
     event Blacklisted(address addr, bool status);
+
+    constructor(address _aggregator) public {
+    	priceFeed = AggregatorV3Interface(_aggregator);
+    }
+
+    function getLatestEthPrice() public view returns (int256) {
+    	(uint80 roundID, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) = priceFeed.latestRoundData();
+    	return answer;
+    }
+
+    function whitelistAddress(address _member) public payable {
+    	//whitelist the member once sntch tokens have been purchased
+    }
+
+    function () external payable {
+    	//fallback function if ether is sent to the DAO without any specific function called
+    }
 }
