20e8b0d5270e936f0ae67c326dc069229bbb6940
tunde <icepaigez@gmail.com>
2021-09-09 12:00:40 +0100

    feat: fixed the buy token function of the sntch DAO

--- src/contracts/SntchDao.sol
+++ src/contracts/SntchDao.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./interfaces/SntchTokenInterface.sol";
 
-contract SntchDao {
+contract SntchDao is Ownable {
 
 	AggregatorV3Interface internal priceFeed;
 	SntchTokenInterface public sntch;
@@ -13,7 +13,7 @@ contract SntchDao {
 	using SafeMath for uint256;
 
 	string public name = 'SNTCH DAO';
-	uint256 public tokenPriceInWei = 2500000000000000000; //$2.5 in wei int256 (NOT uint256: note the difference because of the chainlink price datatype)
+	uint256 public sntchTokenPrice = 250000000000000000000000000; //$2.5/token * 10**8(from chainlink eth/usd price) *10**18(toWei)  
 	uint256 public whitelistedNumber = 0;
 
 	mapping (address => bool) whitelist;
@@ -24,15 +24,15 @@ contract SntchDao {
     event TokenAddressChange(address token);
 
     constructor(address _aggregator, address _sntchTokenAddress) public {
-    	require(_sntchTokenAddress != address(0x0), "Token address cannot be null-address");
+    	require(_sntchTokenAddress != address(0x0), "Token address cannot be a null-address");
     	priceFeed = AggregatorV3Interface(_aggregator);
     	sntch = SntchTokenInterface(_sntchTokenAddress);
     }
 
     function getCurrentTokenPrice() public view returns (uint256) {
     	(,int256 answer, , ,) = priceFeed.latestRoundData();
-    	uint256 price = uint256 (answer);
-    	uint256 minTokenPurchaseInEth = tokenPriceInWei / price;
+    	uint256 price = uint256(answer);
+    	uint256 minTokenPurchaseInEth = sntchTokenPrice / price;
     	return minTokenPurchaseInEth;
     }
 
@@ -40,7 +40,7 @@ contract SntchDao {
     	//whitelist the member once sntch tokens have been purchased
     	require(!whitelist[_add], "Candidate must not be already whitelisted");
     	require(!blacklist[_add], "Candidate must not be already blacklisted");
-    	require(msg.value >= getCurrentTokenPrice(), "You must have enough for at least one sntch token which is approx. $2.5 worth of Ether");
+    	require(msg.value >= getCurrentTokenPrice(), "You must send enough Ether for at least one sntch token which is approx. $2.5 worth of Ether");
     
     	//buyTokens(_add, msg.value);
     	whitelist[_add] = true;
@@ -48,12 +48,48 @@ contract SntchDao {
     	emit Whitelisted(_add, true);
     }
 
-    fallback () external payable {
+    function buyTokensThrow(address _buyer) external payable {
+    	require(!whitelist[_buyer], "Candidate must not be already whitelisted");
+    	require(!blacklist[_buyer], "Candidate must not be already blacklisted");
+
+    	uint256 minTokenPurchaseInEth = getCurrentTokenPrice();
+    	require(msg.value >= minTokenPurchaseInEth, "You must send enough Ether for at least one sntch token which is approx. $2.5 worth of Ether");
+    	uint256 tokensToPurchase = msg.value / minTokenPurchaseInEth;
+    	require(daoTokenBalance() >= tokensToPurchase);
+    	sntch.transfer(_buyer, tokensToPurchase);
+    }
+
+    //this is called if there are no tokens left for sale in the DAO; the payment is refunded
+    function buyTokensInternal(address _buyer, uint256 _amount) internal {
+    	require(!blacklist[_buyer], "Candidate must not be already blacklisted");
+    	uint256 minTokenPurchaseInEth = getCurrentTokenPrice();
+    	require(_amount >= minTokenPurchaseInEth, "You must send enough Ether for at least one sntch token which is approx. $2.5 worth of Ether");
+    	uint256 tokensToPurchase = _amount / minTokenPurchaseInEth;
+    	if (daoTokenBalance() < tokensToPurchase) { //refund the ether that was sent
+    		address payable sender = payable(msg.sender);
+    		sender.transfer(_amount);
+    	} else {
+    		sntch.transfer(_buyer, tokensToPurchase);
+    	}
+    }
+
+    fallback() external payable {
     	//fallback function if ether is sent to the DAO without any specific function called
     	if (!whitelist[msg.sender]) {
     		whitelistAddress(msg.sender);
     	} else {
     		//buyTokens(msg.sender, msg.value);
     	}
+    }
+
+    function daoTokenBalance() public view returns (uint256) {
+    	return sntch.balanceOf(address(this));
+    }
+
+    //needed incase the token needs to be changed
+    function changeTokenAddress(address _newToken) onlyOwner public {
+    	require(_newToken != address(0x0), "Token address cannot be a null-address");
+    	sntch = SntchTokenInterface(_newToken);
+    	emit TokenAddressChange(_newToken);
     }
 }
